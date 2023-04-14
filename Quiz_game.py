383c701a66da79eb979152ad94f6e5f835a90ed2
confused1439 <saradvavivek@gmail.com>
2023-04-14 17:38:18 +0530

    Done

---
+++ Quiz_game.py
@@ -0,0 +1,56 @@
+print("Welcome to my Quiz game!")
+
+playing = input("Do you want to play? ")
+
+# Condition
+if playing != 'yes':
+    quit()
+
+print("Okay! Let's play :)")
+
+# Question no.1
+answer = input("What does CPU stands for? ")
+
+if answer == 'Central Processing Unit':
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+# Question 1
+answer = input("What does CPU stands for? ")
+
+if answer == 'Central Processing Unit':
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+# Question 2
+answer = input("What does GPU stands for? ")
+
+if answer == 'Graphic Processing Unit':
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+# Question 3
+answer = input("What does RAM stands for? ")
+
+if answer == 'Random Access Memory':
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+# Question
+answer = input("What does CPU stands for? ")
+
+if answer == 'Central Processing Unit':
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does CPU stands for? ")
+
+if answer == 'Central Processing Unit':
+    print("Correct!")
+else:
+    print("Incorrect!")

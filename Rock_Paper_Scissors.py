5f7f98f58535add34949fd19c4e39b780171759d
confused1439 <saradvavivek@gmail.com>
2023-04-17 20:02:11 +0530

    Score adjustments

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,43 +1,59 @@
 import random
 
-user_moves = {"Rock": 0, "Paper": 1, "Scissors": 2}
+user_score = 0
+computer_score = 0
 computer_moves = {0: "Rock", 1: "Paper", 2: "Scissors"}
+
+# Function to take input from user
 
 
 def generate_computer_moves():
     r = random.randint(0, 2)
 
-    return r
+    return computer_moves[r]
+
+# Function to take input from computer
 
 
 def generate_user_moves():
     answer = input("Enter the move: ")
-
-    answer_value = user_moves[answer]
-
-    return answer_value
+    return answer
 
 
-def judge(user, computer):
+ans = 'y'
 
-    user_score = 0
-    computer_score = 0
+while ans == 'y':
 
-    if user == computer:
-        print("Tie")
-    elif user > computer:
-        print("You win!")
-        user_score += 1
-    elif user < computer:
-        print("Computer win!")
-        computer_score += 1
-    return user_score, computer_score
-
-
-while True:
     user_turn = generate_user_moves()
-
     computer_turn = generate_computer_moves()
 
-    print(user_turn, "\n", computer_turn)
-    print(judge(user_turn, computer_turn))
+    print(user_turn)
+    print(computer_turn)
+
+    if user_turn == computer_turn:
+        print("Tie")
+        user_score += 1
+        computer_score += 1
+    elif user_turn == 'Paper' and computer_turn == 'Rock':
+        print("You win!")
+        user_score += 1
+    elif user_turn == 'Rock' and computer_turn == 'Scissors':
+        print("You win!")
+        user_score += 1
+    elif user_turn == 'Scissors' and computer_turn == 'Paper':
+        print("You win!")
+        user_score += 1
+    elif user_turn == 'Rock' and computer_turn == 'Paper':
+        print("Computer win!")
+        computer_score += 1
+    elif user_turn == 'Scissors' and computer_turn == 'Rock':
+        print("Computer win!")
+        computer_score += 1
+    elif user_turn == 'Paper' and computer_turn == 'Scissors':
+        print("You win!")
+        computer_score += 1
+
+    ans = input("\nDo you want to play more? (y/n) ")
+
+print("\nYour Score:", user_score)
+print("Computer Score:", computer_score)
